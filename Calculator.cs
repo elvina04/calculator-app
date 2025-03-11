9ac1d22a20c73362e20132261a2d78e12ece0197
elvina04 <elvina@students.amikom.ac.id>
2025-03-11 14:17:31 +0700

    Tugas 4 : Melengkapi Fungsi Main

--- Calculator.cs
+++ Calculator.cs
@@ -2,7 +2,15 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
+	
+		int a = 10;
+		int b = 6;
 		
+		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2})", a, b, Penambahan(a, b));
+		Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2})", a, b, Pengurangan(a, b));
+		
+		Console.WriteLine("\nTekan sembarangan key untuk keluar");
+		Console.ReadKey();
 		
 	}
 	
@@ -15,4 +23,5 @@ class Calculator
 	{
 		return a - b;
 	}
+	
 }
